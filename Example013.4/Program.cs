afe02b4fa7f7ff585efc16f801b63c37d0789041
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-12 11:35:25 +0300

    add Example 013.4

---
+++ Example013.4/Program.cs
@@ -0,0 +1,15 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2) 
+
+double Fibonachi(int n)
+{   // 1! = 1
+    // 0! = 1
+    if(n == 1 || n == 2) return 1;
+    else return Fibonachi(n-1) + Fibonachi(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f{i} = {Fibonachi(i)}");
+}
